f58fa352ed214d04c79f3bd085da62de84d27ebd
Johan Andersson <johan.andersson@rubrik.com>
2025-07-23 17:35:50 +0200

    Relax the version constraint for the Azure RM provider

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.14.0"
+      version = ">=4.14.0"
     }
     azapi = {
       source  = "Azure/azapi"
@@ -11,7 +11,7 @@ terraform {
     }
     polaris = {
       source  = "rubrikinc/polaris"
-      version = "=>1.1.3"
+      version = ">=1.1.3"
     }
   }
 }
